741ee5cf793b56bc5164cbfb757841955f134115
IBMADMIN <IBMADMIN@LAPTOP-5DPV2C32.ibmbarclays.com>
2021-10-10 00:34:11 +0530

    added product and prodct scree basic setup with css without redux

--- frontend/src/Components/screens/Homescreen/Homescreen.js
+++ frontend/src/Components/screens/Homescreen/Homescreen.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Product from '../../Product/Product'
+import './Homescreen.css'
 const Homescreen = () => {
   return (
     <div className="homescreen">
+      <h2 className="homescreen__title"> Latest Product</h2>
+      <div className="homescreen__products">
+<Product/>
+<Product/>
+<Product/>
+<Product/>
+<Product/>
+<Product/>
+<Product/>
+<Product/>
+      </div>
       Home screen 
     </div>
   )
--- frontend/src/Components/screens/Productscreen/Productscreen.js
+++ frontend/src/Components/screens/Productscreen/Productscreen.js
@@ -2,9 +2,32 @@ import React from 'react'
 import './Productscreen.css'
 const Productscreen = () => {
   return (
-    <div className="productScreen">
-      Hi product screen
+    <div className="productscreen">
+      <div className="productscreen__left">
+        <div className="left__image">
+          <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="Product name" />
+        </div>
+        <div className="left__info">
+          <p className="left__name">Product 1</p>
+          <p>Price</p>
+          <p>Description: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, ipsam?</p>
+        </div>
+      </div>
+      <div className="productscreen__right">
+        <div className="right__info">
+          <p>Price <span>234</span></p>
+          <p>Status <span>In Stock</span></p>
+          <p>Quantity <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select></p>
+          <p><button type="button">Add To cart</button></p>
+        </div>
+      </div>
     </div>
+
   )
 }
 
