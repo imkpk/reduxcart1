721070d380d0a2819b74bb1a7c6c0d27e226fa6a
IBMADMIN <IBMADMIN@LAPTOP-5DPV2C32.ibmbarclays.com>
2021-10-11 23:32:35 +0530

    added cartitem and completed css

--- frontend/src/Components/screens/Cartscreen/Cartscreen.js
+++ frontend/src/Components/screens/Cartscreen/Cartscreen.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import Cartitem from "../../Cartitem/Cartitem";
+import "./Cartscreen.css";
 
 const Cartscreen = () => {
   return (
-    <div>
-      Cart Screen
+    <div className="cartscreen">
+      <div className="cartscreen__left">
+        <h2>Shopping Cart</h2>
+        <Cartitem />
+        <Cartitem />
+        <Cartitem />
+        <Cartitem />
+        <Cartitem />
+        <Cartitem />
+        <Cartitem />
+      </div>
+      <div className="cartscreen__right">
+        <div className="cartscreen__info">
+          <p>subtotal (0) items:</p>
+          <p>$499</p>
+        </div>
+        <div><button>Proceed to checkout</button></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cartscreen
+export default Cartscreen;
